05eda9f2f97abfdc6a19de4a03733be559d47e19
Gir31_PC <rpg2657@gmail.com>
2024-10-07 21:02:08 +0900

    가장 가까운 점 찾기 구현 완료

--- ex_01_11_2021184002.py
+++ ex_01_11_2021184002.py
@@ -11,63 +11,81 @@ coords = [
 	(594, 494), (576, 702), (64, 218)
 ]
 
-def dist(a, b):
-	d = 1#???
+coords.sort(key=lambda t:t[0])
+x_sorted = [(coords[i][0], coords[i][1], i) for i in range(len(coords))]
+y_sorted = sorted(x_sorted, key=lambda t:t[1])
 
+def dist(a, b): 
+	x1, y1 = a
+	x2, y2 = b
+	return (x1 - x2)**2 + (y1 - y2)**2
 
-def dist_sq(a, b):
-	d = 1#???
+def brute_force(arr, coord1, coord2):
 
-def brute_force(arr, i1, i2):
-	md = ???
-	for x: # i1 ~ 12
-		for ??: #x+1 ~ i2
+	s, e = coord1, coord2
+	md = dist(arr[coord1], arr[coord2])
+	
+	for start in range(coord1, coord2 + 1):
+		for end in range(start + 1, coord2 + 1):
+			d = dist(arr[start], arr[end])
 			if d < md:
 				md = d
-				s = 
-				e =
-
-	return s, e, d
+				s = start
+				e = end
+	return s, e, md
 
 
 def dnc(arr, i1, i2):
-	switch size:
-		<= 1: return -1, -1, 0
-		== 2: return i1, i2, dist(i1, i2)
-		== 3: return brute_force(arr, i1, i2)
+	size = i2 - i1 + 1
+
+	if size == 1: 
+		return -1, -1, 0
+	elif size == 2: 
+		return i1, i2, dist(arr[i1], arr[i2])
+	elif size == 3: 
+		s, e, d = brute_force(arr, i1, i2)
+		return s, e, d
 	
-	# size >= 4
-	s1, e1, d1 = dnc(...)
-	s2, e2, d2 = dnc(...)
+	mid_index = size // 2 + i1 - 1
+
+	s1, e1, d1 = dnc(arr, i1, mid_index)
+	s2, e2, d2 = dnc(arr, mid_index + 1, i2)
 
 	if d1 < d2:
-		??
+		return s1, e1, d1
 	else:
-		???
+		return s2, e2, d2
 
-	d 결정
+def dnc_all(arr, i1, i2):
+	s, e, d = dnc(arr, i1, i2)
 
-	x좌표: 중간점x좌표-d ~ 중간점 x좌표+d
-	index1: x좌표가 중간점x좌표-d 이상인 점들 중 가장 왼쪽 index
-	index2: x좌표가 중간점x좌표+d 이하인 점들 중 가장 오른쪽 index
-	strip = [t for t in y_sorted if t[2] >= index1 and t[2] <= index2]
+	mid_index = (i2 + i1)//2 - 1
+	left_index, right_index = i1, i2
 
+	for i in range(mid_index, i1, -1):
+		ld = (arr[mid_index][0] - arr[i][0])**2
+		if ld <= d: left_index = i
+		else: break
 
+	for i in range(mid_index, i2, 1):
+		rd = (arr[mid_index][0] - arr[i][0])**2
+		if rd <= d: right_index = i
+		else: break
 
+	strip = [t for t in y_sorted if t[2] >= left_index and t[2] <= right_index]
 
-print(coords)
-coords.sort(key=lambda t:t[0])
+	for i in range(len(strip)):
+		for j in range(i + 1, len(strip)):
+			dis = dist(arr[i], arr[j])
+			if dis < d:
+				s, e, d = i, j, dis
+			else:
+				break
 
-x_sorted = [(coords[i][0], coords[i][1], i) for i in range(len(coords))]
+		
 
-y_sorted = sorted(x_sorted, key=lambda t:t[1])
+	return s, e, d
 
-x_sorted = sorted(coords, ....)
-
-#s, e, d = brute_force(coord, 0, len(coords) - 1)
-s, e, d = dnc(coords, 0, len(coords) - 1)
-
-
-#print(f'{s=} {e=} {d=}')
+s, e, d = dnc_all(coords, 0, len(coords) - 1)
 
 print(f'[{s}]{coords[s]} - [{e}]{coords[e]}, {d=}')
